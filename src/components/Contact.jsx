ef1da343a31b9ea17ba8d0a50d30aeae08fe0ffe
Anupesh Kumar Verma <Anupeshkverma121@gmail.com>
2024-01-20 00:50:12 +0530

    Portfolio Done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       name="contact"
       className="w-full bg-gradient-to-b from-black to-gray-800 p-4 text-white pt-16"
     >
-      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
@@ -14,7 +14,7 @@ const Contact = () => {
           <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
 
-        <div className=" flex justify-center items-center">
+        <div className="flex justify-center items-center">
           <form
             action="https://getform.io/f/7d2cbbe5-ab81-403a-9f1a-e381a9efe054"
             method="POST"
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import heroImage from "../assets/heroImage.jpg";
 import { FaCircleArrowRight } from "react-icons/fa6";
-// import { Link } from "react-scroll";
+import { Link } from "react-scroll";
 
 const Home = () => {
   return (
@@ -9,7 +9,7 @@ const Home = () => {
       name="home"
       className="md:h-screen w-full bg-gradient-to-b from-black via-black to-gray-500"
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row gap-5 items-center justify-center  md:h-full pt-20 p-4 ">
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row gap-8 items-center justify-center  md:h-full pt-20 p-4 ">
         <div id="sideText" className="flex flex-col justify-center md:h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             I'm a Full Stack Developer
@@ -19,12 +19,17 @@ const Home = () => {
             experience in building websites
           </p>
           <div>
-            <button className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link
+              to="projects"
+              smooth
+              duration={500}
+              className=" group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300 ml-2">
                 <FaCircleArrowRight size={20} />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div id="heroImage">
